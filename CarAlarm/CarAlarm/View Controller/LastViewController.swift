e0a0418a8302320ff2064f2cf72530d2a32731a4
Voooovka <vovkavysochanskyi@gmail.com>
2021-05-02 23:04:35 +0300

    show users location

--- CarAlarm/CarAlarm/View Controller/LastViewController.swift
+++ CarAlarm/CarAlarm/View Controller/LastViewController.swift
@@ -5,10 +5,15 @@
 //  Created by Володимир Височанський on 11.04.2021.
 //
 
+import CoreLocation
+import MapKit
 import UIKit
 
-class LastViewController: UIViewController {
+class LastViewController: UIViewController, CLLocationManagerDelegate {
     
+    @IBOutlet var mapView: MKMapView!
+    
+    let manager = CLLocationManager()
     
     @IBOutlet weak var homeButton: UIButton!
     
@@ -25,6 +30,38 @@ class LastViewController: UIViewController {
         setUpElements()
         // Do any additional setup after loading the view.
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.delegate = self
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first{
+            manager.stopUpdatingLocation()
+            
+            render(location)
+        }
+    }
+    
+    func render(_ location: CLLocation) {
+        let coordinate = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+
+        let span = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+
+        let region = MKCoordinateRegion(center: coordinate, span: span)
+
+        mapView.setRegion(region, animated: true)
+
+        let pin = MKPointAnnotation()
+        pin.coordinate = coordinate
+        mapView.addAnnotation(pin)
+    }
+    
+    
     func setUpElements() {
         
         // Hide the error label
